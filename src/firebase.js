006b0c40be6709c6327c71e855e3f15e2ddf91ec
AdiVk2003 <117760549+AdiVk2003@users.noreply.github.com>
2024-01-13 20:07:36 +0530

    Updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,29 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
+import "firebase/compat/firestore"
+import { addDoc, collection } from "firebase/firestore";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyCRHc9OJ7LDNPn6BfPBqsh3BFSP56yl2bQ",
     authDomain: "llg0-6e768.firebaseapp.com",
     projectId: "llg0-6e768",
-    storageBucket: "llg0-6e768.appspot.com",
+    storageBucket: "gs://llg0-6e768.appspot.com",
     messagingSenderId: "471632806453",
     appId: "1:471632806453:web:ef14a77b00d10136c8a77c"
-      };
-const app=firebase.initializeApp(firebaseConfig )
-export const auth=app.auth()
-export default app
+};
+
+const app=firebase.initializeApp(firebaseConfig);
+export const auth=app.auth();
+export const db=firebase.firestore(app);
+const storage = getStorage(app);
+
+export const uploadImage = async (file) => {
+  const storageRef = ref(storage, `images/${file.name}`);
+  await uploadBytes(storageRef, file);
+  return getDownloadURL(storageRef);
+};
+
+export const WriteUsers = (lawyers, data) => addDoc(collection(db, lawyers), data);
+export default app;
