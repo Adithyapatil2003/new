304d12fbb0909514d1815353b2e90e051c1ef6f2
AdiVk2003 <117760549+AdiVk2003@users.noreply.github.com>
2024-01-13 20:09:23 +0530

    Updated index.jsx

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -1,15 +1,37 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
-
-import { Button, Img, Input, Line, List, Text } from "components";
-import LandingPageFooter from "components/LandingPageFooter";
-import LandingPageHeader from "components/LandingPageHeader";
+import ContentToImport from './form.jsx';
+import { useNavigate } from 'react-router-dom';
+import {Img, Line, Text, Button } from "components";
 
 const ContactPagePage = () => {
+
+  const history =useNavigate();
+  const handleBack = (event) => {
+    event.preventDefault();
+    history('/list')
+  };
+
   return (
     <>
-      <div className="bg-gray-51 flex flex-col font-markoone sm:gap-10 md:gap-10 gap-[100px] items-start justify-start mx-auto w-auto sm:w-full md:w-full">
+      <div className="bg-gray-51 flex flex-col sm:gap-10 md:gap-10 gap-[100px] items-start justify-start mx-auto w-auto sm:w-full md:w-full">
         <div className="flex flex-col md:gap-10 gap-[60px] items-start justify-start w-full">
-          <LandingPageHeader className="bg-white-A700 flex gap-2 h-20 md:h-auto items-center justify-between md:px-5 px-[120px] py-[19px] w-full" />
+
+          <div className="bg-white-A700 flex gap-2 h-20 md:h-auto items-center justify-between md:px-5 px-[120px] py-[19px] w-full">
+            <div className="flex md:flex-col flex-row md:gap-10 items-center justify-between w-full">
+              <div className="flex flex-col gap-[1px]">
+                <Text className="text-orange-A700 text-xl" size="txtMarkoOneRegular20">lawfind</Text>
+                <Text size="txtManropeSemiBold18Gray900">by LegalLawGuru</Text>
+              </div>
+            </div>
+            <div className="flex flex-row gap-10 h-[42px] md:h-auto sm:hidden items-center justify-start w-[150px]">
+              <Button onClick={handleBack}
+              className="bg-gray-900 cursor-pointer font-manrope font-semibold py-2.5 rounded-[10px] text-base text-center text-white-A700 w-full">
+                back
+              </Button>
+            </div>
+          </div>
+
           <div className="flex flex-col font-manrope items-center justify-start md:px-10 sm:px-5 px-[120px] w-full">
             <div className="flex flex-col gap-11 items-center justify-start max-w-[1200px] mx-auto w-full">
               <div className="flex flex-col gap-4 items-center justify-start w-full">
@@ -17,16 +39,15 @@ const ContactPagePage = () => {
                   className="sm:text-[40px] md:text-[46px] text-[54px] text-center text-gray-900 tracking-[-1.08px] w-full"
                   size="txtManropeExtraBold54"
                 >
-                  Get in touch
+                  Welcome Esquire!
                 </Text>
                 <Text
                   className="leading-[180.00%] max-w-[1200px] md:max-w-full text-center text-gray-600 text-lg"
-                  size="txtManropeRegular18Gray600"
+                  size="txtManropeRegular1Gray600"
                 >
-                  On the other hand, we denounce with righteous indignation and
-                  dislike men who are so beguiled and demoralized by the charms
-                  of pleasure of the moment, so blinded by desire, that they
-                  cannot foresee the pain and trouble.
+                  LawFind revolutionizes legal practice by providing lawyers a seamless platform to connect with clients. 
+                  Lawyers showcase expertise and profiles, streamlining client acquisition. 
+                  The user-friendly interface prioritizes transparency, simplifying the process for lawyers to expand their clientele and efficiently engage with those seeking legal services.
                 </Text>
               </div>
               <div className="bg-white-A700 border border-bluegray-100 border-solid flex md:flex-col flex-row gap-[50px] items-start justify-start p-[50px] md:px-10 sm:px-5 rounded-[10px] w-full">
@@ -36,84 +57,22 @@ const ContactPagePage = () => {
                       className="sm:text-2xl md:text-[26px] text-[28px] text-gray-900 tracking-[-0.56px] w-full"
                       size="txtManropeExtraBold28"
                     >
-                      Send a message
+                      Add Details For Display
                     </Text>
-                    <div className="flex flex-col gap-3 items-start justify-start w-full">
-                      <Input
-                        name="textfieldlarge"
-                        placeholder="Full Name"
-                        className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg w-full"
-                        wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex pl-4 pr-[35px] py-[17px] rounded-[10px] w-full"
-                        type="text"
-                        prefix={
-                          <Img
-                            className="mt-auto mb-px h-6 mr-3.5"
-                            src="images/img_user.svg"
-                            alt="user"
-                          />
-                        }
-                      ></Input>
-                      <Input
-                        name="textfieldlarge_One"
-                        placeholder="Email Address"
-                        className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg w-full"
-                        wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex pl-4 pr-[35px] py-[17px] rounded-[10px] w-full"
-                        type="email"
-                        prefix={
-                          <Img
-                            className="mt-auto mb-px h-6 mr-3.5"
-                            src="images/img_mail_gray_600_24x24.svg"
-                            alt="mail"
-                          />
-                        }
-                      ></Input>
-                      <Input
-                        name="textfieldlarge_Two"
-                        placeholder="Phone Number"
-                        className="font-semibold p-0 placeholder:text-gray-600 sm:pr-5 text-gray-600 text-left text-lg w-full"
-                        wrapClassName="bg-white-A700 border border-bluegray-100 border-solid flex pl-4 pr-[35px] py-[17px] rounded-[10px] w-full"
-                        type="number"
-                        prefix={
-                          <Img
-                            className="mt-auto mb-px h-6 mr-3.5"
-                            src="images/img_call.svg"
-                            alt="call"
-                          />
-                        }
-                      ></Input>
-                      <div className="bg-white-A700 border border-bluegray-100 border-solid flex flex-col h-[152px] md:h-auto items-start justify-start px-[19px] py-3.5 rounded-[10px] w-full">
-                        <Text
-                          className="text-gray-600 text-lg w-auto"
-                          size="txtManropeSemiBold18Gray600"
-                        >
-                          Message
-                        </Text>
-                      </div>
-                    </div>
+                    <ContentToImport />
                   </div>
-                  <Button className="bg-gray-900 cursor-pointer font-semibold py-[17px] rounded-[10px] text-base text-center text-white-A700 w-full">
-                    Send Request
-                  </Button>
                 </div>
-                <Line className="bg-bluegray-100 h-[534px] md:h-px md:w-full w-px" />
+                <Line className="bg-bluegray-100 h-[720px] md:h-px md:w-full w-px" />
                 <div className="flex flex-1 flex-col gap-10 items-start justify-center w-full">
                   <div className="flex flex-col gap-4 items-start justify-start w-full">
                     <div className="flex flex-col gap-1 items-start justify-start w-full">
-                      <Text
-                        className="text-gray-900 text-xl tracking-[-0.40px] w-auto"
-                        size="txtManropeSemiBold20Gray900"
-                      >
-                        Office Address
-                      </Text>
-                      <Text
-                        className="leading-[180.00%] text-gray-600 text-lg"
-                        size="txtManropeSemiBold18Gray600"
-                      >
-                        <>
-                          1421 San Pedro St, Los Angeles, <br />
-                          CA 90015
-                        </>
-                      </Text>
+                    <Text
+                    className="text-orange-A700 text-xl w-auto"
+                    size="txtMarkoOneRegular20"
+                  >
+                    LawFind
+                  </Text>
+                  <Text size="txtManropeSemiBold18Gray900">by LegalLawGuru</Text>
                     </div>
                     <div className="flex flex-col gap-2.5 items-start justify-start w-full">
                       <div className="flex flex-row gap-3 items-center justify-start w-full">
@@ -126,7 +85,7 @@ const ContactPagePage = () => {
                           className="flex-1 text-gray-600 text-lg w-auto"
                           size="txtManropeSemiBold18Gray600"
                         >
-                          (123) 456-7890
+                          (+91) 98452 36784
                         </Text>
                       </div>
                       <div className="flex flex-row gap-3 items-center justify-start w-full">
@@ -135,48 +94,52 @@ const ContactPagePage = () => {
                           src="images/img_mail_gray_600_24x24.svg"
                           alt="mail_One"
                         />
-                        <Text
-                          className="flex-1 text-gray-600 text-lg w-auto"
-                          size="txtManropeSemiBold18Gray600"
+                        <a
+                        href="mailto:contactus@legallawguru.com"
+                        className="flex-1 text-gray-600 text-lg w-auto"
+                        size="txtManropeSemiBold18Gray600"
                         >
-                          info@mail.com
-                        </Text>
+                          contactus@legallawguru.com
+                        </a>
                       </div>
                     </div>
                   </div>
-                  <div className="flex flex-col gap-[13px] items-start justify-start w-full sm:w-full">
-                    <Text
-                      className="text-gray-900 text-xl tracking-[-0.40px] w-full"
+                  <div className="flex flex-col gap-4 items-start justify-start w-full">  
+                  <Text
+                      className="text-gray-900 text-xl tracking-[-0.40px] mb-2"
                       size="txtManropeSemiBold20Gray900"
                     >
-                      Social
+                      Social and other links
                     </Text>
-                    <div className="flex flex-row gap-4 items-start justify-start w-full">
-                      <Img
-                        className="h-[30px] w-[30px]"
-                        src="images/img_clock_gray_600.svg"
-                        alt="clock"
-                      />
-                      <Img
+                  </div>
+                  <div className="flex flex-row gap-4 items-start justify-start w-full">
+                    <Img
                         className="h-[30px] w-[30px]"
                         src="images/img_linkedin_gray_600.svg"
                         alt="linkedin"
                       />
-                      <Img
+                       <a
+                        href="https://www.linkedin.com/company/legallawguru-llg/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-black-500 ml-2"
+                      >
+                        LegalLawGuru
+                      </a>
+                    <div className="flex flex-row gap-4 items-start justify-start w-full">
+                      <img
                         className="h-[30px] w-[30px]"
-                        src="images/img_twitter_gray_600.svg"
-                        alt="twitter"
+                        src="https://img.icons8.com/pastel-glyph/64/webpages--v2.png"
+                        alt="webpages--v2"
                       />
-                      <Img
-                        className="h-[30px] w-[30px]"
-                        src="images/img_play.svg"
-                        alt="play"
-                      />
-                      <Img
-                        className="h-[30px] w-[30px]"
-                        src="images/img_signal.svg"
-                        alt="signal"
-                      />
+                      <a
+                        href="https://legallawguru.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-black-500 ml-2"
+                      >
+                        LegalLawGuru
+                      </a>
                     </div>
                   </div>
                 </div>
@@ -184,7 +147,7 @@ const ContactPagePage = () => {
             </div>
           </div>
         </div>
-        <LandingPageFooter className="bg-white-A700 flex gap-2 items-center justify-center md:px-5 px-[120px] py-20 w-full" />
+        <p className="mb-4"></p>
       </div>
     </>
   );
